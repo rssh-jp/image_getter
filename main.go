b3abee42ba35fbd5b11b91aeb58cdd31536766f1
rssh-jp <t-araumi@rssh.sakura.ne.jp>
2018-03-22 18:41:04 +0900

    commit

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import(
     "log"
     "io"
     "os"
+    "path/filepath"
     "net/http"
+    "net/url"
     "strings"
+    "sync"
     "github.com/PuerkitoBio/goquery"
 )
 
-var mapUrl map[string]bool
 
 func test(url string){
     res, err := goquery.NewDocument(url)
@@ -29,7 +31,7 @@ func test(url string){
             log.Fatal(e)
          }
          defer r.Body.Close()
-         file, e := os.Create("/tmp/rsrc/" + name)
+         file, e := os.Create(config.StoragePath + name)
          if e != nil{
             log.Fatal(e)
          }
@@ -40,47 +42,100 @@ func test(url string){
     fmt.Println("-----------------------------")
     res.Find("a").Each(func(_ int, s *goquery.Selection) {
          wkUrl, _ := s.Attr("href")
-         getImageByUrl(wkUrl)
+         recursiveGetImage(wkUrl, 0)
     })
 }
 
-func getImageByUrl(url string){
-    _, ok := mapUrl[url]
-    if ok {
+func copy(name string, src io.Reader)(err error){
+    file, err := os.Create(name)
+    if err != nil{
+        fmt.Println("os.Create err. : ", err)
         return
     }
-    mapUrl[url] = true
-    fmt.Println("-----------------------------")
-    fmt.Println(url)
-    res, err := goquery.NewDocument(url)
+    defer file.Close()
+
+    _, err = io.Copy(file, src)
+    if err != nil{
+        fmt.Println("io.Copy err. : ", err)
+        return
+    }
+    return
+}
+func saveImage(url, dir string)(err error){
+    l := strings.Split(strings.Split(url, "?")[0], "/")
+    name := l[len(l) - 1]
+    r, err := http.Get(url)
+    if err != nil{
+       fmt.Println(err)
+       return
+    }
+    defer r.Body.Close()
+
+    path := filepath.Join(dir, name)
+    err = os.MkdirAll(dir, 0755)
+    if err != nil{
+        fmt.Println("os.MkdirAll err. : ", err)
+        return
+    }
+
+    err = copy(path, r.Body)
     if err != nil{
         fmt.Println(err)
         return
     }
-    count := 0
+    fmt.Printf("Done copy. %s to %s\n", url, path)
+    return
+}
+
+func getDir(u *url.URL)string{
+    list := strings.Split(strings.Trim(u.Path, "/"), "/")
+    str := strings.Join(list[:len(list) - 1], "_")
+    dir := filepath.Join(config.StoragePath, u.Host, str)
+    return dir
+}
+
+func recursiveGetImage(urlStr string, count int){
+    if count > config.Depth{
+        return
+    }
+
+    _, ok := mapUrl[urlStr]
+    if ok {
+        return
+    }
+    mapUrl[urlStr] = true
+
+    res, err := goquery.NewDocument(urlStr)
+    if err != nil{
+        return
+    }
+
+    netUrl, _ := url.Parse(urlStr)
+    fmt.Println(netUrl)
     res.Find("img").Each(func(_ int, s *goquery.Selection) {
-        url, _ := s.Attr("src")
-        l := strings.Split(strings.Split(url, "?")[0], "/")
-        name := l[len(l) - 1]
-        fmt.Printf("%v : %v\n", count, name)
-        r, e := http.Get(url)
-        if e != nil{
-           fmt.Println(err)
-           return
+        urlStr, _ := s.Attr("src")
+        workUrl, err := netUrl.Parse(urlStr)
+        if err != nil{
+            fmt.Println("URL.Parse err. : ", err)
+            return
         }
-        defer r.Body.Close()
-        file, e := os.Create("/tmp/rsrc/" + name)
-        if e != nil{
-           fmt.Println(err)
-           return
+
+        _, ok := mapUrl[workUrl.String()]
+        if ok {
+            return
         }
-        defer file.Close()
-        io.Copy(file, r.Body)
-        count++
+
+        if err := saveImage(workUrl.String(), getDir(workUrl)); err !=nil{
+            fmt.Println("saveImage err. : ", err)
+            return
+        }
+
+        mapUrl[workUrl.String()] = true
     })
     res.Find("a").Each(func(_ int, s *goquery.Selection) {
          wkUrl, _ := s.Attr("href")
-         getImageByUrl(wkUrl)
+         workUrl, _ := netUrl.Parse(wkUrl)
+         recursiveGetImage(workUrl.String(), count + 1)
     })
 }
 
@@ -107,7 +162,9 @@ func getFileString(path string)(ret string, err error){
 
 // コンフィグ
 type Config struct{
-    Url string
+    Url string  `json:"url"`
+    StoragePath string   `json:"storage_path"`
+    Depth int   `json:"depth"`
 }
 func getConfigByString(str string)(ret Config){
     dec := json.NewDecoder(strings.NewReader(str))
@@ -128,27 +185,39 @@ func getConfig(path string)(ret Config, err error){
     ret = getConfigByString(str)
     return
 }
-func main(){
-    var config Config
+const(
+    chNum = 10
+)
+var(
+    config Config
+    mapUrl = make(map[string]bool)
+    ch = make(chan struct{}, chNum)
+    mutex sync.Mutex
+)
+func init(){
+    var err error
     var path string
+
     flag.StringVar(&path, "path", "", "config.json path")
     flag.Parse()
     if path == ""{
-        config, err := getConfig("config.json")
+        config, err = getConfig("config.json")
         if err != nil{
             log.Fatal(err)
         }
-        fmt.Println(config)
     }else{
-        config, err := getConfig(path)
+        config, err = getConfig(path)
         if err != nil{
             log.Fatal(err)
         }
-        fmt.Println(config)
     }
-    
-    //mapUrl = make(map[string]bool)
-    //url := "http://www.idea-webtools.com/2014/01/night-view-wallpaper.html"
-    //getImageByUrl(url)
+}
+func main(){
+    recursiveGetImage(config.Url, 0)
+
+    fmt.Println("end")
+    for key, _ := range mapUrl{
+        fmt.Println(key)
+    }
 }
 
